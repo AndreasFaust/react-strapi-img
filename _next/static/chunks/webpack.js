abff3c7f36ea9b979a83ca4d5b559142eaf17e48
Andreas Faust <test@andreass-mbp.speedport.ip>
2020-12-24 12:06:53 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0864745d694cb2c0b9c2";
+/******/ 	var hotCurrentHash = "f71a73bcd96069baf08f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
